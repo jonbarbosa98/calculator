eb7943658137017b8841c550b9b2121255e4c711
Jonilson Barbosa <jon.barbosa98@gmail.com>
2023-01-19 20:36:23 -0300

    Adicionando todos os arquivos

---
+++ script.js
@@ -0,0 +1,42 @@
+let btnNumber = document.querySelectorAll(".calc_number");
+let calc_screen = document.querySelector(".calc-screen");
+let clear = document.getElementById("calc-clear");
+let result = "";
+let arrayString = [];
+
+clear.onclick = ()=>{
+	result = "";
+	arrayString = [];
+	calc_screen.innerHTML = "";
+}
+function calculate(e){
+	let number = result;
+	result = "";
+
+	if (e==="=") {
+		arrayString.push(number);
+		let total = eval(arrayString.join(""));
+
+		calc_screen.innerHTML = total;
+		arrayString = [];
+	}
+	arrayString.push(number);
+	arrayString.push(e); 
+}
+
+btnNumber.forEach((item)=>{
+	item.onclick = (e)=> {
+		let text = e.target.innerText;
+		if (result === "0") {
+			result = "0";
+		}
+		if (result.length<=15) {
+			result += text;
+		}
+		if (result.length >= 8) {
+			calc_screen.style.fontSize = "25px";
+		}
+
+		calc_screen.innerHTML = result;
+	}
+})
